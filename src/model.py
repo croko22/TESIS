0be6a628778fd6e3cae77c15c99e906c96c48132
croko22 <kvchambit@gmail.com>
2025-06-18 22:19:18 -0500

    improve model

--- src/model.py
+++ src/model.py
@@ -1,7 +1,16 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
 
-def load_trained_model(model_path: str):
-    print(f"Cargando modelo desde: {model_path}")
-    model = T5ForConditionalGeneration.from_pretrained(model_path)
-    tokenizer = T5Tokenizer.from_pretrained(model_path)
-    return model, tokenizer
+class QuestionGeneratorModel:
+    def __init__(self, model_name="t5-base"): 
+        self.model_name = model_name
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+
+    def get_tokenizer(self):
+        return self.tokenizer
+
+    def get_model(self):
+        return self.model
